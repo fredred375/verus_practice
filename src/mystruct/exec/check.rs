461808a91a5bea75bf29436eb1156357bf61cc28
Frederick Chu <fhchu2@illinois.edu>
2025-05-01 00:09:45 -0500

    fix: remove logs

--- src/mystruct/exec/check.rs
+++ src/mystruct/exec/check.rs
@@ -26,13 +26,24 @@ pub fn check_mystruct(mystruct: &exec_types::MyStruct) -> (res: bool)
                 idx <= f.len(),
                 result == forall |i: int| 0 <= i < idx ==> spec_types::state_validation(f[i]@),
         {
-            result = result && exec_types::state_validation(&f[idx]);
+            if exec_types::state_validation(&f[idx]) == false {
+                result = false;
+            }
             idx += 1;
         }
+        // 1. assert result is correct for f
         assert(result == (forall |i: int| 0 <= i < f.len() ==> spec_types::state_validation(f[i]@)));
+        // 2. assert f[i]@ is equal to mystruct@.field.get_Some_0()[i] for all i
         assert(forall |i: int| 0 <= i < mystruct@.field.get_Some_0().len() ==> (mystruct@.field.get_Some_0()[i] == f[i]@));
+        // 3. assert f length is equal to original
         assert(mystruct@.field.get_Some_0().len() == f.len());
+        // 4. assert again f[i]@ is equal to mystruct@.field.get_Some_0()[i] for all i with len substituted
         assert(forall |i: int| 0 <= i < f.len() ==> (mystruct@.field.get_Some_0()[i] == f[i]@));
+        assert(forall |i: int| 0 <= i < f.len() ==> (
+            spec_types::state_validation(f[i]@)
+            == spec_types::state_validation(mystruct@.field.get_Some_0()[i])
+        ));
+        // 5. substitute f[i]@ with mystruct@.field.get_Some_0()[i] into 1. since by 2. and 4. f[i]@ is equal to mystruct@.field.get_Some_0()[i]
         // assert(result == (forall |i: int| 0 <= i < f.len() ==> spec_types::state_validation(mystruct@.field.get_Some_0()[i])));
         result
     } else {
